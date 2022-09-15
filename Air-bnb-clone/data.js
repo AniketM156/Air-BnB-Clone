2c1fddf4421ea72bd395703e7cbb056c6e46ffa5
Aniket Mahadik <64539262+AniketM156@users.noreply.github.com>
2022-09-15 20:50:26 +0530

    Add files via upload

---
+++ Air-bnb-clone/data.js
@@ -0,0 +1,107 @@
+export default [
+    {
+        id: 1,
+        title: "Mountain Biking Classes",
+        description: "Experience the beautiful landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike providedas well!)",
+        price: 50,
+        coverImg: "mountain-bike.png",
+        stats: {
+            rating: 4.2,
+            reviewCount: 5
+        },
+        location: "India",
+        openSpots: 15,
+    },
+    {
+        id: 2,
+        title: "Indian classical dance",
+        description: "Learn Indian classical dances like katha while making freinds on the way and improving your health",
+        price: 75,
+        coverImg: "Dance.jpg",
+        stats: {
+            rating: 4.7,
+            reviewCount: 25
+        },
+        location: "India",
+        openSpots: 30,
+    },
+    {
+        id: 3,
+        title: "Ice skiing",
+        description: "Learn Ice skiing from the bset skiers and enjoy your time as you learn while befreinding fellow skiers.",
+        price: 125,
+        coverImg: "ice-sking.jpg",
+        stats: {
+            rating: 4.5,
+            reviewCount: 16
+        },
+        location: "russia",
+        openSpots: 10,
+    },
+    {
+        id: 4,
+        title: "Roller skating",
+        description: "Learn how to ride with the wind with your roller skates(Roller skates provided!)",
+        price: 70,
+        coverImg: "Skating.jpg",
+        stats: {
+            rating: 4.1,
+            reviewCount: 8
+        },
+        location: "Ofline",
+        openSpots: 10,
+    },
+    
+    {
+        id: 5,
+        title: "swiming Lessons from Katie Zaferes",
+        description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+        price: 136,
+        coverImg: "katie-zaferes.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        location: "Online",
+        openSpots: 0,
+    },
+    {
+        id: 6,
+        title: "Zumba",
+        description: "Learn the new dance form at the comfort of your home",
+        price: 80,
+        coverImg: "Zumba.jpg",
+        stats: {
+            rating: 4.0,
+            reviewCount: 21
+        },
+        location: "Online",
+        openSpots: 20,
+    },
+    {
+        id: 7,
+        title: "Wedding Photography Bootcamp",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 175,
+        coverImg: "wedding-photography.png",
+        stats: {
+            rating: 4.2,
+            reviewCount: 40
+        },
+        location: "Online",
+        openSpots: 27,
+    },
+    {
+        id: 8,
+        title: "Karate",
+        description: "Learn the ancient combat skill of Japan called as Karate , while discipling your self and health.",
+        price: 95,
+        coverImg: "Karate.jpg",
+        stats: {
+            rating: 4.9,
+            reviewCount: 46
+        },
+        location: "Ofline",
+        openSpots: 30,
+    }
+]
